55a0b1947d2f6ea849a96274e6fe2ff4a055d1bf
Bravin Murega <tedmurega@gmail.com>
2024-12-10 10:23:54 +0300

    changed the route to dashboard.view_dashboard

--- app/Routes/authentication.py
+++ app/Routes/authentication.py
@@ -106,7 +106,7 @@ def login_farmer():
             response = make_response(jsonify({
                 "login": "sucess"
             })), 201
-            response = make_response(redirect('/dashboard '))
+            response = make_response(redirect(url_for('dashboard.view_dashboard')))
             response.set_cookie(
                 "session_token",
                 access_token,
